a4a92354f207062ea3bd7be6dccd7d1752bf8331
Chinmayi <chinmaybhatt234@gmail.com>
2023-11-29 11:00:36 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
@@ -36,6 +37,6 @@ def predict():
 if __name__=="__main__":
     app.run(debug=True)
 
-    
+
 
     
